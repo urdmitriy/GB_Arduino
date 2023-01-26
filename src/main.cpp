01ef8722846d034fe3fb03f1bc1dfb20a5984aba
Uryupin <urdmitriy@yandex.ru>
2023-01-26 20:43:18 +0300

    Home work 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,81 @@
 #include <Arduino.h>
 
-#define pinLedRed 10
-#define pinLedGreen 6
-#define pinLedBlue 5
-#define pinResistor A0
-#define delayColor 3000
+#define pinSER 10
+#define pinRCLK 8
+#define pinSRCLK 9
 
-uint8_t ledArray[3] = {pinLedRed, pinLedGreen, pinLedBlue};
-unsigned long timeStamp = millis();
-uint8_t numberActiveLed = 0;
+void sendTo595(uint16_t value){
+  for (size_t i = 0; i < 9; i++)
+  {
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(pinLedRed, OUTPUT);
-  pinMode(pinLedGreen, OUTPUT);
-  pinMode(pinLedBlue, OUTPUT);
-}
-
-uint16_t readData(void){
-  static int32_t valuePhotoSensor = 0;
-  valuePhotoSensor += (analogRead(pinResistor) - valuePhotoSensor) * 0.1;
-  return valuePhotoSensor*255/1023;
-}
-
-void lightLed(uint8_t pinLed, uint8_t valueLight){
-
-  if (pinLed == pinLedRed) {
-    analogWrite(pinLedRed, valueLight); 
-    analogWrite(pinLedBlue, 0); 
-    analogWrite(pinLedGreen, 0);
-  } else if (pinLed == pinLedGreen) {
-    analogWrite(pinLedGreen, valueLight); 
-    analogWrite(pinLedBlue, 0); 
-    analogWrite(pinLedRed, 0);
-  } else{
-    analogWrite(pinLedBlue, valueLight); 
-    analogWrite(pinLedGreen, 0); 
-    analogWrite(pinLedRed, 0);
+    if (value & 0x01 << (8-i)){
+      digitalWrite(pinSER, HIGH);
+    }
+    else {
+      digitalWrite(pinSER, LOW);
+    }
+    
+    digitalWrite(pinSRCLK,HIGH);
+    delayMicroseconds(1);
+    digitalWrite(pinSRCLK,LOW);
+    delayMicroseconds(1);
+    
   }
 
+  digitalWrite(pinRCLK,HIGH);
+  delayMicroseconds(1);
+  digitalWrite(pinRCLK,LOW);
+  delayMicroseconds(1);
+}
+
+void printDigit(uint8_t value){
+  switch (value)
+  {
+  case 0:
+    sendTo595(0b00111111);
+    break;
+  case 1:
+    sendTo595(0b00000110);
+    break;
+  case 2:
+    sendTo595(0b01011011);
+    break;
+  case 3:
+    sendTo595(0b01001111);
+    break;
+  case 4:
+    sendTo595(0b01100110);
+    break;
+  case 5:
+    sendTo595(0b01101101);
+    break;
+  case 6:
+    sendTo595(0b01111101);
+    break;
+  case 7:
+    sendTo595(0b00000111);
+    break;
+  case 8:
+    sendTo595(0b01111111);
+    break;
+  case 9:
+    sendTo595(0b01101111);
+    break;
+  default:
+    break;
+  }
+}
+
+void setup() {
+  pinMode(pinSER, OUTPUT);
+  pinMode(pinRCLK, OUTPUT);
+  pinMode(pinSRCLK, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  if (millis() - timeStamp > delayColor){
-    if (numberActiveLed<2){
-      numberActiveLed++;
-    }
-    else{
-      numberActiveLed = 0;
-    }
-    timeStamp = millis();
+  for (size_t i = 0; i < 10; i++)
+  {
+    printDigit(i);
+    delay(500);
   }
-  
-  lightLed(ledArray[numberActiveLed], readData());
 }
